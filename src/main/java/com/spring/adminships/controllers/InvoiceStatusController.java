4f637e4c913f85f4528ef0a9d5f06e4ae32e8bbe
KenanDawa <72620388+kinanDawa@users.noreply.github.com>
2021-08-19 13:27:14 +0300

    refactoring package names

--- src/main/java/com/spring/adminships/controllers/InvoiceStatusController.java
+++ src/main/java/com/spring/adminships/controllers/InvoiceStatusController.java
@@ -1,4 +1,4 @@
-package com.kindsonthegenius.fleetapp.controllers;
+package com.spring.adminships.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
