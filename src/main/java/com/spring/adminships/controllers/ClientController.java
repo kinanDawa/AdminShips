265227eb8a0416241f65412dea5ab99deee339e1
KenanDawa <72620388+kinanDawa@users.noreply.github.com>
2021-08-26 14:45:07 +0300

    Adding the Controllers

---
+++ src/main/java/com/spring/adminships/controllers/ClientController.java
@@ -0,0 +1,12 @@
+package com.spring.adminships.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientController {
+	@GetMapping("/clients")
+	public String getClients() {
+		return "Client";
+}
+}
---
+++ src/main/java/com/spring/adminships/controllers/JobTitleController.java
@@ -0,0 +1,14 @@
+package com.spring.adminships.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JobTitleController {
+	
+	@GetMapping("/jobtitles")
+	public String getJobTitle() {
+		return "JobTitle";
+	}
+
+}
